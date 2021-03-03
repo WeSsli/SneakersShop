6d782e552c3d8489f098494c9d501f69e784e073
WeSsli <vvatrixx@gmail.com>
2021-03-03 14:30:55 +0100

    Precache images

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:sneakers/screens/login_screen.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:sneakers/utils/precache_image.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.white,
+    ),
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
+        builder: (context, widget) {
+          PrecacheImages().init(context);
+          return ResponsiveWrapper.builder(
+            BouncingScrollWrapper.builder(context, widget),
+            maxWidth: 1200,
+            minWidth: 450,
+            defaultScale: true,
+            breakpoints: [
+              ResponsiveBreakpoint.resize(450, name: MOBILE),
+              ResponsiveBreakpoint.autoScale(800, name: TABLET),
+              ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+              ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+              ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+            ],
+          );
+        });
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: SizedBox(
+                child: SvgPicture.asset('assets/images/LoginImage.svg'),
+                height: 200,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              "Logowanie",
+              style: GoogleFonts.quicksand(
+                fontSize: 38,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
